997d6e286144ac77cc2de56e04834360c867c6d8
Darren <Darren.Su@ecs.com.tw>
2019-09-24 17:53:11 +0800

    [add] use matplotlib draw learning curve
    GradientDescentOptimizer=0.05
    layer1 = Neurons 10
    layer2 = Neurons 10

--- Neural_network.py
+++ Neural_network.py
@@ -9,6 +9,8 @@ import numpy as np
 from add_layer import add_layer as al
 import tensorflow as tf
 
+import matplotlib.pyplot as plt
+
 #create test data
 # [:,np.newaxis]為列轉換成行[300,]->[300,1]
 x_data= np.linspace(-1,1,300)[:,np.newaxis]
@@ -23,6 +25,15 @@ y_data=np.square(x_data)-0.5+noise
 xs= tf.placeholder(tf.float32,[None,1])
 ys= tf.placeholder(tf.float32,[None,1])
 
+#plot input data
+fig= plt.figure()
+#add plot
+ax=fig.add_subplot(1,1,1)
+ax.scatter(x_data,y_data)
+plt.ion()
+plt.show()
+
+
 #use relu activation method
 l1= al(xs,1,10,activation_function=tf.nn.relu)
 
@@ -30,16 +41,36 @@ l2= al(l1,10,10,activation_function=tf.nn.relu)
 
 result= al(l2,10,1,activation_function=None)
 
+
+#calculate loss value
 loss= tf.reduce_mean( tf.reduce_sum( tf.square(ys-result),
                                     reduction_indices=[1]))
 
-train_step= tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+train_step= tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+
 
 inti= tf.global_variables_initializer()
-
 with tf.Session() as sess:
     sess.run(inti)
     for i in range(1000):
         sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
         if i %50 ==0:        
-            print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+            #print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+            try:
+                #remove frist lines if presence
+                ax.lines.remove(lines[0])
+            except Exception:
+                pass
+            #create train line
+            plot_value= sess.run(result,feed_dict={xs:x_data,ys:y_data})
+            lines=ax.plot(x_data,plot_value, 'r-', lw=5)
+            
+            #pause 0.1 sec
+            plt.pause(0.1)
+
+
+
+
+
+                
