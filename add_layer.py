d4e4fde6fe567874e708f73d31c8c402c29cede3
Darren <Darren.Su@ecs.com.tw>
2019-09-24 15:59:08 +0800

    [create] add_layer for hide layer function

---
+++ add_layer.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 24 13:25:12 2019
+
+@author: DarrenSu
+"""
+
+import tensorflow as tf
+
+def add_layer(inputs,in_size,out_size,activation_function=None):
+    Weights= tf.Variable(tf.random_normal([in_size,out_size]))
+    biases= tf.Variable(tf.zeros([1,out_size])+0.1)
+    
+    Wx_plus_b= tf.matmul(inputs,Weights)+biases
+    
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+        
+    return outputs
