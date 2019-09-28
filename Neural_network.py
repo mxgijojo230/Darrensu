e6cae8945c0c12ab5b5b81e4378b81347a817f66
mxgijojo230 <mxgijojo230@gmail.com>
2019-09-28 18:27:34 +0800

    [create] TensorBoard function,
    use chrome show training rate

--- Neural_network.py
+++ Neural_network.py
@@ -16,47 +16,54 @@ import matplotlib.pyplot as plt
 x_data= np.linspace(-1,1,300)[:,np.newaxis]
 #add noise
 noise= np.random.normal(0,0.05,x_data.shape).astype(np.float32)
-
+#y_data=np.square( np.linspace(-1,1,300)[:,np.newaxis])+noise
 #np.square = arr**2
 y_data=np.square(x_data)-0.5+noise
 
 #create tf.placeholder()
-
-xs= tf.placeholder(tf.float32,[None,1])
-ys= tf.placeholder(tf.float32,[None,1])
+with tf.name_scope('inputs'):
+    xs= tf.placeholder(tf.float32,[None,1],name='x_in')
+    ys= tf.placeholder(tf.float32,[None,1],name='y_in')
 
 #plot input data
 fig= plt.figure()
 #add plot
 ax=fig.add_subplot(1,1,1)
 ax.scatter(x_data,y_data)
+
 plt.ion()
 plt.show()
-
 
 #use relu activation method
 l1= al(xs,1,10,activation_function=tf.nn.relu)
 
-l2= al(l1,10,10,activation_function=tf.nn.relu)
+l2= al(l1,10,5,activation_function=tf.nn.relu)
 
-result= al(l2,10,1,activation_function=None)
+result= al(l2,5,1,activation_function=None)
 
 
 #calculate loss value
-loss= tf.reduce_mean( tf.reduce_sum( tf.square(ys-result),
-                                    reduction_indices=[1]))
-
-
-train_step= tf.train.GradientDescentOptimizer(0.05).minimize(loss)
+with tf.name_scope('Loss'):
+    loss= tf.reduce_mean( tf.reduce_sum( tf.square(ys-result),
+                                    axis= 1,name='sum'),name= 'mean')
+    tf.summary.scalar('Loss',loss)
+    
+with tf.name_scope('Train'):
+    train_step= tf.train.GradientDescentOptimizer(0.05).minimize(loss)
 
 
 inti= tf.global_variables_initializer()
 with tf.Session() as sess:
+    writer=tf.summary.FileWriter('logs/',sess.graph)
+    merged=tf.summary.merge_all()
+    #merged =tf.summary.merge([lossw,lossw])
     sess.run(inti)
     for i in range(1000):
         sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
         if i %50 ==0:        
-            #print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
+            #display histogram
+            rs = sess.run(merged,feed_dict={xs:x_data,ys:y_data})
+            writer.add_summary(rs, i)
             try:
                 #remove frist lines if presence
                 ax.lines.remove(lines[0])
@@ -68,7 +75,7 @@ with tf.Session() as sess:
             
             #pause 0.1 sec
             plt.pause(0.1)
-
+            
 
 
 
--- add_layer.py
+++ add_layer.py
@@ -8,14 +8,21 @@ Created on Tue Sep 24 13:25:12 2019
 import tensorflow as tf
 
 def add_layer(inputs,in_size,out_size,activation_function=None):
-    Weights= tf.Variable(tf.random_normal([in_size,out_size]))
-    biases= tf.Variable(tf.zeros([1,out_size])+0.1)
     
-    Wx_plus_b= tf.matmul(inputs,Weights)+biases
-    
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-        
-    return outputs
+    with tf.name_scope('layer'):
+        with tf.name_scope('Weights'):
+            Weights= tf.Variable(tf.random_normal([in_size,out_size]),name= 'W')
+            tf.summary.histogram(' ',Weights)
+        with tf.name_scope('biases'):    
+            biases= tf.Variable(tf.zeros([1,out_size])+0.1)
+            tf.summary.histogram(' ',biases)
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b= tf.matmul(inputs,Weights)+biases
+            tf.summary.histogram(' ',Wx_plus_b)
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)          
+            tf.summary.histogram('outputs',outputs)           
+        return outputs
+
